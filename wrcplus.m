3289315c5f72ef50667b00195755c27e17420e97
Christopher Jung <cjung@bu.edu>
2021-04-21 19:11:21 -0400

    update

--- wrcplus.m
+++ wrcplus.m
@@ -1,7 +1,21 @@
 function w = wrcplus(X, y, lambda)
-    X = X(:, 1:11);
-    %X = normalize(X,'scale');
-    %y = normalize(y,'scale');
+    %{
+    %Normalizes the data
+    m_X=mean(X);
+    s_X=std(X);
+
+    %mu_mat=repmat(m_X,size(X,1),1);
+    s_mat_X=repmat(s_X,size(X,1),1);
+    X=(X-m_X)./s_mat_X;
+    
+    m_y=mean(y);
+    s_y=std(y);
+
+    %mu_mat_y=repmat(m_y,size(y,1),1);
+    s_mat_y=repmat(s_y,size(y,1),1);
+    y=(y-m_y)./s_mat_y;
+    %}
+
     [m1, d1] = size(X);
     [m2, d2] = size(y);
     
@@ -22,95 +36,86 @@ function w = wrcplus(X, y, lambda)
         w = [];
         return;
     end
-    %X = [ones(m1,1),X];
-    %converg_tol = 1e-4; % convergence tolerance
+
     I = eye(d1);
     %initial w (X'*X + lambda*I) * (X'*y)  
     A = X'*X + lambda*I;
     B = X'*y;
-    %w = pinv(A) * B; %initializing w, way of doing the inverse
-    w = zeros(d1,1);
+    w = pinv(A) * B; %initializing w, way of doing the inverse
+    %w = zeros(d1,1); %for error check
     
     
-    %wrc_w = zeros(m2,d2);
     r = zeros(d1,1);
-    %least_sq = zeros(1,m1);
     conv = 0; %converging limit
-    %a = zeros(1,m1);
-    %c = zeros(1,m1);
     p = 1;
-    %w = [];
     obj = zeros(1, d1);
     least_sq = zeros(d1,1); 
     loss = [];
     loss2 = [];
-    while conv < 50 
+    
+    while conv < 100 
         
         for i = 1:d1
-            %r = lambda * norm(w,p); % r is regularizer
-            checkr = size(r)
-            wsize = size(w)
-            xsize = size(X)
-            %least_sq(i) = (norm(y-(X*w))).^2; %Least square 
-            %least_sq = mean((y-X*w).^2)
-            checklstsqr = size(least_sq)
-            %obj(i) = r(i) + least_sq(:,i);
-            %obj(i) = (1/m1)*(r(i) + (least_sq(i)));
-            
-            %a = 2*sum(X(i,:)'.^2);
+
             a = 0;
             for j = 1:m1
                 a = a + 2*(X(j,i).^2);
             end
-            
-               
-            %c = 2*sum(X(i,:)*(y(i) - w*X(i,:) + w(i,:)*X(i,:)));
+
             c = 0;
             for j = 1:m1
                 c = c + 2*(X(j,i)*(y(j) - w'*X(j,:)' + w(i)*X(j,i)));
             end
-            checka = size(a)
-            checkc = size(c)
             
             %soft thresholding
-            %w(i,:) = sign(c(i),a(i)) * max((abs(c(i)/a(i)) - (lambda/a(i))),0);
             w(i) = sign(c/a)*(max((abs(c/a)-(lambda/a)),0));
+            
+            %checking value of objective function
+            %{
+            if (i == 1 && conv == 0)
+                r = lambda * norm(w,p); % r is regularizer
+                least_sq = (norm(y-(X*w))).^2; %Least square 
+                obj_wo =  (r + least_sq);
+            end
+            %}
         end
+        
+        %calculating objective function
         r = lambda * norm(w,p); % r is regularizer
         least_sq = (norm(y-(X*w))).^2; %Least square 
-        obj =  r + least_sq;
+        obj =  (r + least_sq);
         
-        
-        %objcheck = obj
-        tr_err = least_sq;
-        loss2 = [loss2;tr_err];
-        %tr_err = mean(r) + mean(least_sq);
-        
-        %loss2 = [loss2;tr_err];
+        %loss for objective function
         loss = [loss;obj];
         conv = conv + 1;
         
-        
+        %loss for training error
+        tr_err = (1/m1)*sum((y-(X*w)).^2);
+        loss2 = [loss2;tr_err];
     end
-    %tr_err = mean((X*w-y).^2);
-    y_predict = X*w;
+    
+
     [r,~] = size(loss);
     [r2, ~] = size(loss2);
-    checkloss = size(loss)
+
     figure(1)
     for i = 1:r
         plot(i,loss(i),'*');
-        title("objective lambda = 100")
+        title("objective lambda = 0.01, w = (X'*X + lambda*I) * (X'*y)")
         hold on;
     end
     hold off;
+    
     figure(2)
     for i = 1:r2
         plot(i, loss2(i),'*');
-        title("trr err lambda = 100")
+        title("trr err lambda = 0.01, w = (X'*X + lambda*I) * (X'*y)")
         hold on;
     end
     hold off;
-
+    
+    
+    %disp(obj_wo) %displays objective function
+    %disp(sum(y.^2)) % displays the objective function when w = 0
 
 end
